beea7c23e6a66007d6dd36572099b8a912189dfb
Lbr1 <lbrbrito@gmail.com>
2016-03-13 00:29:11 +0000

    Aposta e Apostador atualizados

--- Aposta.rb
+++ Aposta.rb
@@ -1,64 +1,36 @@
-cass Aposta
-	attr_accessor :valor, :opcao, :odd, :estado
+require_relative'Apostador.rb'
 
-	def initialize(valor,opcao,odd)
+class Aposta < Apostador
+	attr_accessor :valor, :opcao, :odd, :estado, :apostador
+
+	def initialize(valor,opcao,odd,apostador,estado=true)
 		@valor=valor
 		@opcao=opcao
 		@odd=odd
-	end
-	# get and set valor
-	def getValor()
-		return @valor
+		@apostador=apostador
 	end
 
-	def setValor(valor)
-		@valor=valor
-	end
-	# get and set opcao
-	def getOpcao()
-		return @opcao
-	end
-
-	def setOpcao(opcao)
-		@opcao=opcao
-	end
-	#get and set estado
-	def isEstado()
-		return @estado
+	def actualizaApostador(vencedor)
+		@estado=false
+		if vencedor==@opcao
+			@apostador.actualizaDisponivel(@odd * @valor)
+			
+		end
 	end
 
-	def setEstado(estado)
-		@estado=estado
-	end
-
-	#To_string
+	
 
 	def to_String()
-		return "Valor: #{valor}"
+		 "Opcao: #{@opcao}  #{@valor}."
 	end
 
-	def ver()
-		puts "Valor #{valor}"
-		puts "Opcao #{opcao}"
-		puts "Odd #{odd}"	
-	end
-
-	# get and set odd
-	def getOdd()
-		return @odd
-	end
-
-	def setOdd(valor)
-		@odd=valor
-	end
-
+	
 	
 end
 
 
+apostador = Apostador.new("Luisinho","lbrbrito@gmail.com","2356","Lb",999)
+nAposta = Aposta.new(15,'x',1,apostador)
+puts nAposta.to_String
 
-nAposta = Aposta.new(15,'x',1)
-nAposta.ver
-nAposta.setValor(69)
-nValor=nAposta.to_String
-puts "Novo valor #{nValor}"
+
