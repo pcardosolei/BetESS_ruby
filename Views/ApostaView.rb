312ef48938ff92753db86f44bb300de429040f01
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2016-03-16 23:04:11 +0000

    aposta view

--- Views/ApostaView.rb
+++ Views/ApostaView.rb
@@ -1,34 +1,41 @@
 
 class ApostaView
 
-	def initialize
-	end
+  def initialize
+  end
 
-	def criarApostaView(evento)
-		puts toString(evento)
-		puts "Insira a sua opcao."
-		opcao =gets.chomp
-	  puts "Insira o valor da aposta."
-		valor =gets.chomp
-		odd = evento.equipas[opcao]
-		aposta=[valor,opcao,odd]
-	end
+  def criarApostaView(evento)
+    puts toString(evento)
+    puts "Insira a sua opcao."
+    opcao =gets.chomp
+    puts "Insira o valor da aposta."
+    valor =gets.chomp
+    odd = evento.equipas[opcao]
+    aposta=[valor,opcao,odd]
+  end
 
-	def verAposta(valor,opcao)
-		"Aposta: #{valor} e opcao #{opcao}"
-	end
+  def verAposta(valor,opcao)
+    "Aposta: #{valor} e opcao #{opcao}"
+  end
 
-	def toString(evento)
-		evento.equipas.each do |equipa, odd|
+  def toString(evento)
+    evento.equipas.each do |equipa, odd|
       puts "#{equipa} --> #{odd}"
         end
-	end
+  end
 
-	  def mostraApostas(evento)
+    def mostraApostas(evento)
     evento.apostas.each do |id, aposta|
       puts "#{aposta.opcao} - #{aposta.valor}"
-    	end
-  	end
+      end
+    end
 
+    def verEstadoApostas(evento,apostador)
+      evento.apostas.each do |id, aposta|
+        if aposta.apostador = apostador
+            puts "#{aposta.opcao} |-| #{aposta.valor} |-| #{aposta.odd} |-| #{aposta.estado}"
+         end
+      end
+    end
 
 end
