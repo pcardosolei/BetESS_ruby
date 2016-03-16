5ea7fbb66a01a0b1ea88d8b270225f56358a4c20
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2016-03-16 23:03:19 +0000

    estados das apostas

--- Controllers/ApostaController.rb
+++ Controllers/ApostaController.rb
@@ -3,28 +3,32 @@ require_relative'../Views/ApostaView.rb'
 
 class ApostaController
 
-	attr_accessor :apostaView,:aposta
+  attr_accessor :apostaView,:aposta
 
-	def initialize
-		@view=ApostaView.new
-	end
+  def initialize
+    @view=ApostaView.new
+  end
 
 # Precisamos do evento para completar -> odd e temos de passar o evento
-	def criarAposta(apostador,evento)
-		lista=@view.criarApostaView(evento)
-		aposta=Aposta.new(lista[0],lista[1],lista[2],apostador)
-		evento.apostas[evento.apostas.length] = aposta
-	end
+  def criarAposta(apostador,evento)
+    lista=@view.criarApostaView(evento)
+    aposta=Aposta.new(lista[0],lista[1],lista[2],apostador)
+    evento.apostas[evento.apostas.length] = aposta
+  end
 
-	def actualizaApostador(vencedor)
-		@aposta.estado=false
-		if vencedor==@aposta.opcao
-			@aposta.apostador.actualizaDisponivel(@aposta.odd * @aposta.valor)
-		end
-	end
+  def actualizaApostador(vencedor)
+    @aposta.estado=false
+    if vencedor==@aposta.opcao
+      @aposta.apostador.actualizaDisponivel(@aposta.odd * @aposta.valor)
+    end
+  end
 
   def mostraApostas(evento)
     @view.mostraApostas(evento)
+  end
+
+  def verEstadoApostas(evento,apostador)
+    @view.verEstadoApostas(evento,apostador)
   end
 
 end
